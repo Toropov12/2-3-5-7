4cfacafc4f86120a50e4de1f36d20eb7cea2a8fb
Dima <dmitrijtoropov1552gmail>
2023-07-26 11:32:14 +0300

    first commit

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+class Advertisements(models.Model):
+    title = models.CharField("Заголовок", max_length=128)
+    discription = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits= 10, decimal_places=2)
+    auction = models.BooleanField("Торг")
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+    update_at = models.DateTimeField("Дата обнавления", auto_now=True)
 # Create your models here.
