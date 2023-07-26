300c8d4f5f6f7ab7f2c7031303063f52d2cec640
Dima <dmitrijtoropov1552gmail>
2023-07-26 11:49:35 +0300

    Home_Work

--- advertisements/settings.py
+++ advertisements/settings.py
@@ -78,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    "2": {
+        'ENGINE': 'django.db.backends.postgersql_psycong2',
+        'NAME': 'djangoproject'
     }
 }
 
