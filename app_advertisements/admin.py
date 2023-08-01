8b3b84e462e116073e251184b96a076d387cd45d
Dima <dmitrijtoropov1552gmail>
2023-08-01 19:14:52 +0300

    Lesson

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Advertisements
+class AdvertisementsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'price', 'created_date', 'auction']
+    list_filter = ['auction', 'created_at']
+admin.site.register(Advertisements, AdvertisementsAdmin)
+
 
 # Register your models here.
--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,9 +1,25 @@
 from django.db import models
+from django.contrib import admin
 class Advertisements(models.Model):
     title = models.CharField("Заголовок", max_length=128)
-    discription = models.TextField("Описание")
+    description = models.TextField("Описание")
     price = models.DecimalField("Цена", max_digits= 10, decimal_places=2)
     auction = models.BooleanField("Торг")
     created_at = models.DateTimeField("Дата создания", auto_now_add=True)
     update_at = models.DateTimeField("Дата обнавления", auto_now=True)
-# Create your models here.
+    '''def __str__
+'''
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold">'
+                'Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+class Meta:
+    db_table = 'advertisements'
+
